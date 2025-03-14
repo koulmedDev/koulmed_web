bc710333806292f09c487ac3dcf02a556c4bd182
hackdave21 <davalawi77@gmail.com>
2025-03-14 11:57:15 +0000

    separation du footer, header et le layout

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="logo-container">
+      <img src="{{asset('images/logo.png')}}" alt="Koulmed Logo" class="logo">
+    </div>
+
+    <div class="hamburger" onclick="toggleMenu()">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+
+    <ul class="nav-menu" id="navMenu">
+      <li><a href="#">Accueil</a></li>
+      <li><a href="#">Nos Services</a></li>
+      <li><a href="#">À propos de nous</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+
+    <div class="header-buttons">
+      <button class="btn-rdv">Prendre un rendez-vous</button>
+      <button class="btn-connect">Se connecter</button>
+    </div>
+  </header>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Koulmed</title>
+  <link rel="icon" href="{{asset('images/logo.png')}}" type="image/x-icon">
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+  <!-- Header -->
+    @include('components.header')
+     <!-- Header -->
+
+  <!--  contenu  -->
+  <main>
+   @yield('content')
+  </main>
+
+  <!-- Footer -->
+  @include('components.footer')
+   <!-- Footer -->
+
+  <script src="{{asset('js/script.js')}}"></script>
+</body>
+</html>
