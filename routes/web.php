75c6b91f6f495e98a7f5ee65079d1c70bba0637e
hackdave21 <davalawi77@gmail.com>
2025-03-15 12:14:29 +0000

    integration du panel admin, segmentation en plusieurs partie,  etablissement des routes pour chaque vue

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/admin/login', function () {
+    return view('admin.login');
+})->name('login.form');
+
+Route::get('/admin/index', function () {
+    return view('admin.index');
+});
